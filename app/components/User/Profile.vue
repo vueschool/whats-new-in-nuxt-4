be6b7b9ac1d34874adec73a0185cc91ef4a86bf1
Daniel Kelly <me@danielkelly.io>
2024-11-06 10:46:51 -0600

    4-normalized-component-names

---
+++ app/components/User/Profile.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { data } = useFetch("/api/user")
+onMounted(() => {
+    console.log('Profile mounted')
+})
+</script>
+<template>
+    <pre>{{ data }}</pre>
+</template>
---
+++ app/pages/normalizedComponentNames.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const show = ref(true);
+</script>
+<template>
+  <div>
+    <KeepAlive :include="['UserProfile']">
+      <UserProfile v-if="show" />
+    </KeepAlive>
+    <button @click="show = !show">Show/Hide</button>
+  </div>
+</template>
