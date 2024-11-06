3dbb2fdf1a623e10b1e1bea3cbfc400e7610d063
Daniel Kelly <me@danielkelly.io>
2024-11-06 09:45:12 -0600

    3-shallow-data-reactivity-in-useasyncdata-and-usefetch

---
+++ app/pages/shallowDataReactivity.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data } = useFetch("/api/user")
+</script>
+<template>
+    <pre>{{ data }}</pre>
+    <div v-if="data">
+        <h1>Shallow Data Reactivity</h1>
+        <p>Name: {{ data.name }}</p>
+        <p>Email: {{ data.email }}</p>
+        <button @click="data.name = 'Dustin Nickerson'">Change Name</button>
+        <button @click="data.email = 'test@test.com'">Change Email</button>
+    </div>
+</template>
---
+++ server/api/user.ts
@@ -0,0 +1,8 @@
+export default defineEventHandler(async (event) => {
+  return {
+    id: 1,
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    avatar: 'https://example.com/avatar'
+  };
+})
