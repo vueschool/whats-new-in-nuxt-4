7bde5f4243d13f58e096fc9315f6ccec891d0880
Daniel Kelly <me@danielkelly.io>
2024-11-07 11:45:44 -0600

    8-shared-directory-for-app-and-server-code

--- app/pages/normalizedComponentNames.vue
+++ app/pages/normalizedComponentNames.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 const show = ref(true);
+console.log(capitalizeFirstLetter('hello from the app'))
+const user: User = {
+  id: 1,
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  avatar: 'https://example.com/avatar'
+}
 </script>
 <template>
   <div>
--- server/api/user.ts
+++ server/api/user.ts
@@ -1,8 +1,10 @@
 export default defineEventHandler(async (event) => {
-  return {
+  console.log(capitalizeFirstLetter('hello from the API'))
+  const user: User = {
     id: 1,
     name: 'John Doe',
     email: 'john.doe@example.com',
     avatar: 'https://example.com/avatar'
   };
+  return;
 })
